e745b6a326bd156db35377a8cdd816a57fcea915
royanb97 <33065311+royanb97@users.noreply.github.com>
2019-01-14 21:49:48 +0100

    Init

---
+++ AnzeigeErstellenServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AnzeigeErstellenServlet{
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response){
+		String = request.getParameter("title");
+		String = request.getParameter("price");
+		String = request.getParameter("description");
+		String = request.getParameter("category");
+
+	}
+}
